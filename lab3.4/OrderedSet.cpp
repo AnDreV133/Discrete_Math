3bae74525da0941f687d08506857e0250ae34ee6
AnDreV133 <dmitrijkubik@gmail.com>
2023-11-01 12:40:38 +0300

    begin writing func getPair

--- lab3.4/OrderedSet.cpp
+++ lab3.4/OrderedSet.cpp
@@ -1,3 +1,13 @@
 #include "OrderedSet.h"
 
+#include <utility>
 
+vector<pair<int, int>> getPairByConditionByDotSet(bool (*condition)(pair<int, int>, pair<int, int>), vector<pair<int, int>> dotSet) {
+    vector<pair<int, int>> res;
+    for (auto& dot1: dotSet) {
+    for (auto& dot2: dotSet) {
+        if(condition(dot1, dot2)) {
+            res.push_back() // todo 
+        }
+    } 
+}
--- test/test.cpp
+++ test/test.cpp
@@ -3,9 +3,29 @@
 //
 
 #include "test.h"
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+vector<pair<int, int>> M1 = {
+    {-1, 1}, {0, 1}, {1, 1},
+    {-1, 0}, {0, 0}, {1, 0}, 
+    {-1, -1}, {0, -1}, {1, -1}};
 
 
 
-void test_lab_3_4(){
+vector<pair<int, int>> M2 = {
+    {0, 2},
+    {-1, 1}, {0, 1},{1, 1},
+    {-2, 0}, {-1, 0}, {0, 0}, {1, 0}, {2, 0},
+    {-1, -1}, {0, -1}, {1, -1},
+    {0, -2}};
 
+bool predicate(pair<int, int> a, pair<int, int> b) {
+    return (a.first + b.second) > (b.first + a.second);
+}
+
+void run_lab_3_4()
+{
 }
