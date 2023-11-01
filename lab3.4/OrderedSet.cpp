a220c90118e1cf934b154e11834b92e1f9759100
AnDreV133 <dmitrijkubik@gmail.com>
2023-11-01 11:11:44 +0300

    add package

---
+++ lab3.4/OrderedSet.cpp
@@ -0,0 +1,3 @@
+#include "OrderedSet.h"
+
+
---
+++ lab3.4/OrderedSet.h
@@ -0,0 +1,12 @@
+//
+// Created by dmitr on 16.10.2023.
+//
+
+#ifndef DISCRETE_MATH_ORDEREDSET_H
+#define DISCRETE_MATH_ORDEREDSET_H
+
+#include "../lab3.1/Relation.h"
+
+
+
+#endif //DISCRETE_MATH_ORDEREDSET_H
